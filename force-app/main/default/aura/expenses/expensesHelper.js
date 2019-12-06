040542932189efca368c0e19d734b4a54259839f
Randy McKissack <randy@mckissack.net>
2019-12-06 18:22:28 -0500

    Added code to expensesJelper.js

--- force-app/main/default/aura/expenses/expensesHelper.js
+++ force-app/main/default/aura/expenses/expensesHelper.js
@@ -1,5 +1,21 @@
 ({
     createExpense: function(component, expense) {
+        var action = component.get("c.saveExpense");
+        action.setParams({
+            "expense": expense
+        });
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var expenses = component.get("v.expenses");
+                expenses.push(response.getReturnValue());
+                component.set("v.expenses", expenses);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    createExpense: function(component, expense) {
         var theExpenses = component.get("v.expenses");
  
         // Copy the expense to a new object
