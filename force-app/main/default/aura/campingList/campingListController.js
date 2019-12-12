9d5c7bbe699029ce9e1cf7b6a23401c7627b66b8
Randy McKissack <randy@mckissack.net>
2019-12-12 10:45:32 -0500

    Camping passed

--- force-app/main/default/aura/campingList/campingListController.js
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -1,31 +1,10 @@
 ({
-    clickCreateItem: function(component, event, helper) {
-        var validCamping = component.find('campingform').reduce(function (validSoFar, inputCmp) {
-            // Displays error messages for invalid fields
-            inputCmp.showHelpMessageIfInvalid();
-            return validSoFar && inputCmp.get('v.validity').valid;
-        }, true);
-        // If we pass error checking, do some real work
-        if(validCamping){
-            // Create the new item
-            var newItem = component.get("v.newItem");
-            console.log("Create camping item: " + JSON.stringify(newItem));
-            helper.createItem(component, newItem);
-            // var theCampingItems = component.get("v.items");
- 
-        // Copy the expense to a new object
-        // THIS IS A DISGUSTING, TEMPORARY HACK
-        // var newItem = JSON.parse(JSON.stringify(newItem));
- 
-        // theCampingItems.push(newItem);
-        // component.set("v.items", theCampingItems);        
-        }
-    },
-
-     // Load list item from Salesforce
-     doInit: function(component, event, helper) {
+    // Load expenses from Salesforce
+    doInit: function(component, event, helper) {
+        
         // Create the action
         var action = component.get("c.getItems");
+        
         // Add callback behavior for when response is received
         action.setCallback(this, function(response) {
             var state = response.getState();
@@ -36,7 +15,24 @@
                 console.log("Failed with state: " + state);
             }
         });
+        
         // Send action off to be executed
         $A.enqueueAction(action);
-    }
+    },
+    
+    handleAddItem: function(component, event, helper) {
+        var newItem = event.getParam("item");
+        var action = component.get("c.saveItem");
+        action.setParams({"item": newItem});
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (component.isValid() && state === "SUCCESS") {
+                var items = component.get("v.items");
+                items.push(item);
+                component.set("v.items",items);
+            }
+        });
+        $A.enqueueAction(action);
+        
+    } 
 })
